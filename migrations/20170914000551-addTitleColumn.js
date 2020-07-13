aae1a304a638c96dbd35097cdd389931207b8eb7
Don Carlos Zendejas <czende@hotmail.com>
2020-07-13 16:14:39 -0500

    Update 20170914000551-addTitleColumn.js

--- migrations/20170914000551-addTitleColumn.js
+++ migrations/20170914000551-addTitleColumn.js
@@ -1,18 +1,1 @@
-'use strict';
 
-module.exports = {
-  up: function (queryInterface, Sequelize) {
-    return queryInterface.addColumn({
-        tableName: 'students',
-      },
-      'title',
-      Sequelize.STRING
-    );
-  },
-
-  down: function (queryInterface, Sequelize) {
-   return queryInterface.removeColumn(
-     'students',
-     'title');
-  }
-};
