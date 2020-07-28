86c5ff85c9394adfe5c6bb30581c739257875115
Don Carlos Zendejas <czende@hotmail.com>
2020-07-28 10:01:38 -0500

    Create robot.js
    create robot

---
+++ public/admin/services/robot.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('admin')
+  .service('Robot', function ($http) {
+
+    var service = {
+
+      getAll: function(){
+          return $http({
+            url: '/admin/schools/all',
+            method:'GET',
+            data:{}
+          });
+      },
+      create: function(data){
+        return $http({
+          url:"/admin/school/create",
+          method:'POST',
+          data:data
+        })
+      },
+      destroy: function(schoolId){
+        return $http({
+          url:"/admin/school/delete",
+          method:'POST',
+          data:{
+            id:schoolId
+          }
+        })
+      }
+    }
+
+    return service;
+
+  });
