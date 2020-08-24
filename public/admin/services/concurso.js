1c3e1b04d79305e16afb2a65f1cc26ab33a897db
Don Carlos Zendejas <czende@hotmail.com>
2020-08-24 14:28:57 -0500

    Update concurso.js

--- public/admin/services/concurso.js
+++ public/admin/services/concurso.js
@@ -7,29 +7,33 @@ angular.module('admin')
 
       getAll: function(){
           return $http({
-            url: '/admin/concursos/all',
+//            url: '/admin/concursos/all',            
+            url:'https://peaceful-retreat-91246.herokuapp.com/banwireapi/cotizaciones101',
             method:'GET',
             data:{}
           });
       },
       create: function(data){
         return $http({
-          url:"/admin/concurso/create",
+//          url:"/admin/concurso/create",
+            url:'https://peaceful-retreat-91246.herokuapp.com/banwireapi/cotizaciones101',          
           method:'POST',
           data:data
         })
       },
       update: function(data){
         return $http({
-          url:"/admin/concurso/edit",
+//          url:"/admin/concurso/edit",
+            url:'https://peaceful-retreat-91246.herokuapp.com/banwireapi/cotizacionesedit101',          
           method:'POST',
           data:data
         })
       },      
       destroy: function(concursoId){
         return $http({
-          url:"/admin/concurso/delete",
-          method:'POST',
+//          url:"/admin/concurso/delete",
+            url:'https://peaceful-retreat-91246.herokuapp.com/banwireapi/cotizaciones101',                    
+          method:'DELETE',
           data:{
             id:concursoId
           }
