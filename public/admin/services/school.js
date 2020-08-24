c23474c585b349bd56d41d6f1b677806f27fa5f5
Don Carlos Zendejas <czende@hotmail.com>
2020-08-24 13:54:33 -0500

    Update school.js

--- public/admin/services/school.js
+++ public/admin/services/school.js
@@ -9,7 +9,7 @@ angular.module('admin')
           console.log('despliega paso 1');
           return $http({
 //            url: '/admin/schools/all',
-            url:'http://peaceful-retreat-91246.herokuapp.com/api/robots',
+            url:'https://peaceful-retreat-91246.herokuapp.com/api/robots',
             method:'GET',
             data:{}
           });
@@ -18,7 +18,7 @@ angular.module('admin')
         console.log('despliega paso 2');
         return $http({
 //          url:"/admin/school/create",
-            url:'http://peaceful-retreat-91246.herokuapp.com/api/robots',          
+            url:'https://peaceful-retreat-91246.herokuapp.com/api/robots',          
           method:'POST',
           data:data
         })
